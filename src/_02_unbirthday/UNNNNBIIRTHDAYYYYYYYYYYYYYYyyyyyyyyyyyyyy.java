d981eb2f4256c84c4903d9ecaf90eed85950b8b2
League Student <league@iMac-24.attlocal.net>
2019-09-08 15:57:13 -0700

    Remarkable

---
+++ src/_02_unbirthday/UNNNNBIIRTHDAYYYYYYYYYYYYYYyyyyyyyyyyyyyy.java
@@ -0,0 +1,59 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class UNNNNBIIRTHDAYYYYYYYYYYYYYYyyyyyyyyyyyyyy {
+public static void main(String[] args) {
+	
+
+
+	String input=JOptionPane.showInputDialog("When is your birthday? (06/07 etc)");
+
+
+if(input.equals ("09/08")) {JOptionPane.showMessageDialog(null, " Happy B-Day Brother!");} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
