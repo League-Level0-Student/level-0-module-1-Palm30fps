922b502bfa03442a470c36746a53c6151d32e55e
League Student <league@iMac-24.attlocal.net>
2019-09-15 15:56:21 -0700

    riddler =) :100:

--- src/_02_unbirthday/UNNNNBIIRTHDAYYYYYYYYYYYYYYyyyyyyyyyyyyyy.java
+++ src/_02_unbirthday/UNNNNBIIRTHDAYYYYYYYYYYYYYYyyyyyyyyyyyyyy.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 
 
 	String input=JOptionPane.showInputDialog("When is your birthday? (06/07 etc)");
-
+z
 
 if(input.equals ("09/08")) {JOptionPane.showMessageDialog(null, " Happy B-Day Brother!");} 
 
