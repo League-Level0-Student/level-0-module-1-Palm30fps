f164335035110c5348434e4cc34f4d81a10751cb
League Student <league@iMac-24.attlocal.net>
2019-09-22 15:59:22 -0700

    zOmBie eyEs

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -5,24 +5,45 @@ import javax.swing.JOptionPane;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
-
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-int score= 0;
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+		String riddle = JOptionPane.showInputDialog(null,
+				"What can travel around the whole wide world while staying in a corner?");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
+		if (riddle.equals("a stamp")) {
+			JOptionPane.showMessageDialog(null, "Correct");
 
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+			score++;
+		}
 
-		// 5. Otherwise, say "wrong" and tell them the answer
-
+// 5. Otherwise, say "wrong" and tell them the answer
+		else {
+			JOptionPane.showMessageDialog(null, "Oops you got it wrong. The right answer was, A stamp!");
+		}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
-		JOptionPane.showMessageDialog(null, score );
+		JOptionPane.showMessageDialog(null, "The score is   "+ score);
+
+		String riddle2 = JOptionPane.showInputDialog(null, "I drink I die I eat I thrive, what am I? ¯\\_(ツ)_/¯  ");
+
+		if (riddle2.equals("fire")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+	score++;	}
+		else {
+			JOptionPane.showMessageDialog(null, "Oops you got it wrong. The right answer was, fire!");
+		}
+	
+		JOptionPane.showMessageDialog(null, "The score is  "+score);
+	
+	
 	}
-}
+	}
+
 
---
+++ src/_09_zombie_eyes/ZOmbie_eyes_yooo_ANrdew_Bilin/ZOmbie_eyes_yooo_ANrdew_Bilin.pde
@@ -0,0 +1,25 @@
+void setup(){
+        size(400,400);  
+PImage face = loadImage("Me.jpg");
+
+face.resize(width, height);
+
+
+image(face, 0, 0);
+    
+  
+  
+}
+
+    void draw(){
+
+ellipse(186, 240, 40, 40);
+   
+ 
+ ellipse(245, 240, 40, 40);
+ 
+ 
+ 
+ }
+                    
+                                 
